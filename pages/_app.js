cf7aa48a72d4c3f2e892943f7d3888cbabdcb8eb
Gabe <gabrielloubier1@gmail.com>
2021-07-06 13:30:22 -0400

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { Toaster } from "react-hot-toast";
+import Navbar from "../components/Navbar";
+import { useUserData } from "../hooks/useUserData";
+import { UserContext } from "../lib/context";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = useUserData();
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
